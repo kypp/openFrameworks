898a6a49f98b9ff7d0d25c71faca8378b7775439
kypp <kyp108@gmail.com>
2015-01-16 14:47:21 +0100

    add configuration for release_profiling
    add static lib build script for windows

---
+++ build_windows.bat
@@ -0,0 +1,4 @@
+call "%programfiles(x86)%\Microsoft Visual Studio 12.0\VC\vcvarsall.bat" x86
+cd libs/openFrameworksCompiled/project/vs
+msbuild /p:Configuration=Release_profiling;Platform=Win32 openframeworksLib.sln
+msbuild /p:Configuration=Release;Platform=Win32 openframeworksLib.sln
