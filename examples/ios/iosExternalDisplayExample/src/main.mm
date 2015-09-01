765bbe6fd1125c6a9cff1e48e15621d4097c89b8
pmjoniak <p.m.joniak@gmail.com>
2015-09-01 13:56:43 +0200

    remove unused addons

--- examples/ios/iosExternalDisplayExample/src/main.mm
+++
@@ -1,8 +0,0 @@
-#include "ofMain.h"
-#include "ofApp.h"
-
-int main(){
-    ofAppiOSWindow * window = new ofAppiOSWindow();
-	ofSetupOpenGL(window, 1024, 768, OF_FULLSCREEN);
-	ofRunApp(new ofApp);
-}
