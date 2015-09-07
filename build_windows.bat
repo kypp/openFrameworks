a30b51acd8b2fee691f7dbab60b33dc971187b71
kyp <kyp108@gmail.com>
2015-09-07 16:50:53 +0200

    add basic cmake
    remove all libraries to enable linking to external library versions
    
    remove some unneeded files

--- build_windows.bat
+++
@@ -1,4 +0,0 @@
-call "%programfiles(x86)%\Microsoft Visual Studio 14.0\VC\vcvarsall.bat" x86
-cd libs/openFrameworksCompiled/project/vs
-msbuild /p:Configuration=Release_profiling;Platform=Win32 openframeworksLib.sln
-msbuild /p:Configuration=Release;Platform=Win32 openframeworksLib.sln
